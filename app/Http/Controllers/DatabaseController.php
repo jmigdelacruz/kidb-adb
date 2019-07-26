e0591da9f505fdcc6b7f0f67ac188c840fb9b4fc
jmigdelacruz <jmig.delacruz@gmail.com>
2019-07-26 11:43:15 +0800

    updated functions

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -9,6 +9,8 @@ use App\SDBSData;
 use App\Country;
 use Spatie\ArrayToXml\ArrayToXml;
 use DB;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class DatabaseController extends Controller
 {
@@ -29,11 +31,20 @@ class DatabaseController extends Controller
     # API home landing page
     public function apiHome()
     {
+        $orderLog = new Logger('api');
+        $orderLog->pushHandler(new StreamHandler(storage_path('logs/api.log')), Logger::INFO);
+        $orderLog->info('api/,'.$this->getUserIpAddr());
         return view('api-home');
     }
 
+    # API home landing page
+    public function test()
+    {
+        return view('test');
+    }
+
     # returns the API XML header
-    public function xmlHeader()
+    public function xmlHeader($per_page,$total_records,$last_page,$curr_page)
     {
         $xml = $this->xml;
         $xml->startElement('message:Header');
@@ -85,6 +96,24 @@ class DatabaseController extends Controller
                 $xml->startElement('message:Extracted');
                 $xml->writeAttribute('text', Carbon::now());
                 $xml->endElement();
+                $xml->startElement('message:Records');
+                if ($total_records) {
+                    $xml->writeAttribute('totalRecords', $total_records);
+                }
+                if ($per_page) {
+                    $xml->writeAttribute('recordsPerPage', $per_page);
+                } else {
+                    $xml->writeAttribute('recordsPerPage', $this->per_page);
+                }
+                if ($total_records && $per_page) {
+                    $xml->writeAttribute('totalPages', number_format($total_records/$per_page,0));
+                } else {
+                    $xml->writeAttribute('totalPages', number_format($total_records/$this->per_page,0));
+                }
+                if ($curr_page) {
+                    $xml->writeAttribute('currentPage', $curr_page);
+                }
+                $xml->endElement();
 
         $xml->endElement();
     }
@@ -93,10 +122,10 @@ class DatabaseController extends Controller
         $dataset = DB::table('SDBS_Metadata')
                        ->leftjoin('SDBS_Subject','SDBS_Metadata.subj_id', '=', 'SDBS_Subject.subj_id')
                        ->leftjoin('SDBS_Country','SDBS_Metadata.ctry_id', '=', 'SDBS_Country.ctry_id')
-                       ->where('SDBS_Subject.subj_sdmx','POP_MID')
+                       ->where('SDBS_Subject.subj_sdmx','MIN_QUR_CNP')
                        // ->where('ctry_sdmx','AF')
                        ->orderBy('ctry_sdmx','asc')
-                       ->select(['SDBS_Subject.subj_id','subj_alt','subj_sdmx','SDBS_Country.ctry_sdmx'])
+                       ->select(['SDBS_Subject.subj_id','subj_alt','subj_sdmx','SDBS_Country.ctry_sdmx','SDBS_Metadata.metadata_id'])
                        ->paginate(10);
         echo "<pre>";
         var_dump($dataset->items());
@@ -140,13 +169,18 @@ class DatabaseController extends Controller
         return $query;
     }
 
+    public function applyAlternateScope($query,$years) {
+        $query->where('SDBS_Metadata.subj_id',$indicator->subj_alt);
+        return $query;
+    }
+
     #filter data by input year
     public function filterData($countries, $years, $indicator) {
         $dataset = DB::table('SDBS_Data')
                        ->leftjoin('SDBS_Metadata','SDBS_Metadata.metadata_id', '=', 'SDBS_Data.metadata_id')
                        ->leftjoin('SDBS_Country','SDBS_Metadata.ctry_id', '=', 'SDBS_Country.ctry_id')
                        ->where('SDBS_Metadata.subj_id',$indicator->subj_id)
-                       ->where('data_year','>=','2001')
+                       ->where('data_year','>=','2000')
                        ->where('data_flg_value','KI')
                        ->orderBy('ctry_sdmx','asc')
                        ->orderBy('data_year','asc')
@@ -154,7 +188,20 @@ class DatabaseController extends Controller
         $dataset = $this->applyCountryScope($dataset,$countries);
         $dataset = $this->applyYearScope($dataset,$years);
         $dataset = $dataset->paginate($this->per_page);
-        return $dataset->items();
+        // if (count($dataset)==0) {
+        //     $dataset = DB::table('SDBS_Data')
+        //                ->leftjoin('SDBS_Metadata','SDBS_Metadata.metadata_id', '=', 'SDBS_Data.metadata_id')
+        //                ->leftjoin('SDBS_Country','SDBS_Metadata.ctry_id', '=', 'SDBS_Country.ctry_id')
+        //                ->where('SDBS_Metadata.subj_id',$indicator->subj_alt)
+        //                ->where('data_year','>=','2000')
+        //                ->orderBy('ctry_sdmx','asc')
+        //                ->orderBy('data_year','asc')
+        //                ->select(['data_value1','data_year','SDBS_Country.ctry_sdmx']);
+        //     $dataset = $this->applyCountryScope($dataset,$countries);
+        //     $dataset = $this->applyYearScope($dataset,$years);
+        //     $dataset = $dataset->paginate($this->per_page);
+        // }
+        return $dataset;
     }
 
     #returns data frequency (annual only)
@@ -184,20 +231,19 @@ class DatabaseController extends Controller
             $datasets     = $this->filterData($request->input('country'), $request->input('year'),$indicator);
             $frequency    = $this->filterFrequency($request->input('freq'));
             $multiplier   = $this->formatMultiplier($request->input('um'));
+        } else { return abort(404); }
+
+        
+        if ($request->input('sdmx_format')=="compact") {
+            return $this->structuredSDMXFormat($sdmx_code, $indicator, $datasets->items(), $frequency, $multiplier, $request->input('input_format'),$request->input('per_page'),$datasets->total(),$datasets->lastPage(),$datasets->currentPage());
         } else {
-            return abort(404);
-        }
-        $input_format = $request->input('input_format');
-        if ($request->input('sdmx_format')=="structured") {
-            return $this->structuredSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format);
-        } else {
-            return $this->genericSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format);    
+            return $this->genericSDMXFormat($sdmx_code, $indicator, $datasets->items(), $frequency, $multiplier,$request->input('input_format'),$request->input('per_page'),$datasets->total(),$datasets->lastPage(),$datasets->currentPage());    
         }
 
     }
 
     #generic format - longer
-    public function genericSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format) {
+    public function genericSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format, $per_page, $total_records, $last_page, $curr_page) {
 
         $xml = $this->xml;
         $xml->openMemory();
@@ -208,7 +254,7 @@ class DatabaseController extends Controller
         $xml->writeAttribute('xmlns:footer', "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message/footer");
         $xml->writeAttribute('xmlns:common', "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common");
         $xml->writeAttribute('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance");
-        $this->xmlHeader();
+        $this->xmlHeader($per_page, $total_records, $last_page, $curr_page);
 
             $xml->startElement('message:DataSet');
             $xml->writeAttribute('action', "Append");
@@ -235,8 +281,26 @@ class DatabaseController extends Controller
                             $xml->writeAttribute('value', "A");
                             $xml->endElement();
                         $xml->endElement();
+                            if ($data->data_value1) {
+                            $xml->startElement('generic:Obs');
+                                $xml->startElement('generic:ObsDimension');
+                                $xml->writeAttribute('id', "TIME_PERIOD");
+                                $xml->writeAttribute('value', $data->data_year);
+                                $xml->endElement();
+                                $xml->startElement('generic:ObsValue');
+                                $xml->writeAttribute('value', $data->data_value1/$multiplier);
+                                $xml->endElement();
+                                // $xml->startElement('generic:Attributes');
+                                //     $xml->startElement('generic:Value');
+                                //     $xml->writeAttribute('id', "UNIT_MULT");
+                                //     $xml->writeAttribute('value', "0");
+                                //     $xml->endElement();
+                                // $xml->endElement();
+                            $xml->endElement();
+                            }   
                         $country = $data->ctry_sdmx;
                     } else {
+                        if ($data->data_value1) {
                         $xml->startElement('generic:Obs');
                             $xml->startElement('generic:ObsDimension');
                             $xml->writeAttribute('id', "TIME_PERIOD");
@@ -245,13 +309,14 @@ class DatabaseController extends Controller
                             $xml->startElement('generic:ObsValue');
                             $xml->writeAttribute('value', $data->data_value1/$multiplier);
                             $xml->endElement();
-                            $xml->startElement('generic:Attributes');
-                                $xml->startElement('generic:Value');
-                                $xml->writeAttribute('id', "UNIT_MULT");
-                                $xml->writeAttribute('value', "0");
-                                $xml->endElement();
-                            $xml->endElement();
+                            // $xml->startElement('generic:Attributes');
+                            //     $xml->startElement('generic:Value');
+                            //     $xml->writeAttribute('id', "UNIT_MULT");
+                            //     $xml->writeAttribute('value', "0");
+                            //     $xml->endElement();
+                            // $xml->endElement();
                         $xml->endElement();
+                        }
                     }
                 }  
             $xml->endElement();
@@ -261,6 +326,12 @@ class DatabaseController extends Controller
         $content = $xml->outputMemory();
         $xml = null;
         $response_time = microtime(true) - LUMEN_START;
+
+        $orderLog = new Logger('api');
+        $orderLog->pushHandler(new StreamHandler(storage_path('logs/api.log')), Logger::INFO);
+        $orderLog->info('querydata,generic,'.$sdmx_code.','.$this->getUserIpAddr());
+
+
         if ($input_format == "json") {
             $xmlObject = simplexml_load_string($content);
             $json = json_encode($xmlObject);
@@ -272,8 +343,8 @@ class DatabaseController extends Controller
         }
     }
 
-    #structure specific - more compact
-    public function structuredSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format) {
+    #structure specific - more compact ///test comment
+    public function structuredSDMXFormat($sdmx_code, $indicator, $datasets, $frequency, $multiplier, $input_format, $per_page, $total_records, $last_page, $curr_page) {
 
         $xml = $this->xml;
         $xml->openMemory();
@@ -285,15 +356,16 @@ class DatabaseController extends Controller
         $xml->writeAttribute('xmlns:footer', "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message/footer");
         $xml->writeAttribute('xmlns:common', "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common");
         $xml->writeAttribute('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance");
-        $this->xmlHeader();
+        $this->xmlHeader($per_page, $total_records, $last_page, $curr_page);
 
         $xml->startElement('message:DataSet');
         $xml->writeAttribute('action', "Append");
         $xml->writeAttribute('structureRef', "KI_DSD_1_0");
 
         $country = "";
-            
+                
                 foreach ($datasets as $data) {
+                    // dd($data);
                     if ($country != $data->ctry_sdmx) {
                         if($country != "") {
                             $xml->endElement();
@@ -302,7 +374,11 @@ class DatabaseController extends Controller
                         $xml->writeAttribute('REF_AREA', $data->ctry_sdmx);
                         $xml->writeAttribute('INDICATOR', $sdmx_code);
                         $xml->writeAttribute('FREQ', "A");
-                        $xml->writeAttribute('UNIT_MULT', "1");
+                        // $xml->writeAttribute('UNIT_MULT', "1");
+                            $xml->startElement('Obs');
+                            $xml->writeAttribute('TIME_PERIOD', $data->data_year);
+                            $xml->writeAttribute('OBS_VALUE', $data->data_value1/$multiplier);
+                            $xml->endElement();
                         $country = $data->ctry_sdmx;
                     } else {
                         $xml->startElement('Obs');
@@ -317,7 +393,11 @@ class DatabaseController extends Controller
         $content = $xml->outputMemory();
         $xml = null;
         $response_time = microtime(true) - LUMEN_START;
-        
+
+        $orderLog = new Logger('api');
+        $orderLog->pushHandler(new StreamHandler(storage_path('logs/api.log')), Logger::INFO);
+        $orderLog->info('querydata,compact,'.$sdmx_code.','.$this->getUserIpAddr());
+
         if ($input_format == "json") {
             $xmlObject = simplexml_load_string($content);
             $json = json_encode($xmlObject);
@@ -327,6 +407,19 @@ class DatabaseController extends Controller
             return response($content)->withHeaders(['Content-Type'=>'text/xml', 'X-Elapsed-Time'=>$response_time]);
             // return response($content)->withHeaders(['Content-Type' => 'text/xml','Content-Disposition' => 'attachment; filename="sampledata.xml"']);
         }
+    }
+
+    public function getUserIpAddr(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //ip pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
 
 }
